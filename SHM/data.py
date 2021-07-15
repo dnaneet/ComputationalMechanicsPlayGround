60b3658355131a2a83712b68f424644e396022e7
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-07-15 08:27:02 -0400

    code and data

--- SHM/data.py
+++ SHM/data.py
@@ -7,7 +7,7 @@ sg = pd.DataFrame(columns = ['time stamp', 'e1', 'e2', 'e3'])
 while i<10:
     sg = sg.append({'time stamp': pd.Timestamp.now(), 'e1': np.random.rand()*100,'e2': np.random.rand()*100, 'e3': np.random.rand()*100}, ignore_index=True)
     i = i + 1
-    time.sleep(10)
+    time.sleep(1)
     print('record #', i)
     #print(sg.iloc[-1:])
 
