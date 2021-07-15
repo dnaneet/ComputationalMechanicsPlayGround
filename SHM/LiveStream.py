ac5d03f3f8e624438280fbeb7d1a2347df3c0345
dnaneet <dnaneet@pop-os.localdomain>
2021-07-15 11:44:31 -0400

    updating data

---
+++ SHM/LiveStream.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Live data stream")
+
+df = pd.read_csv('data.csv')
+df.columns = ['id', 'time stamp', 'e1', 'e2', 'e3']
+
+df[['time stamp', 'e1', 'e2', 'e3']]
