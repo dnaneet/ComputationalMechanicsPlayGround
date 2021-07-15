74527bd999385f9b6ba9c723ad5308162a81c0ed
dnaneet <dnaneet@pop-os.localdomain>
2021-07-15 11:52:11 -0400

    updating data

--- SHM/LiveStream.py
+++ SHM/LiveStream.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 st.title("Live data stream")
-chart = st.linechart(np.random.rand(1,3))
+chart = st.line_chart(np.random.rand(1,3))
 
 df = pd.read_csv('data.csv')
 df.columns = ['id', 'time stamp', 'e1', 'e2', 'e3']
