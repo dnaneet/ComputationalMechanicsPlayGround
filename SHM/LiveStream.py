e11440d9abe4e0c91d2962a0d1d005e34109f2d1
dnaneet <dnaneet@pop-os.localdomain>
2021-07-15 11:51:39 -0400

    updating data

--- SHM/LiveStream.py
+++ SHM/LiveStream.py
@@ -2,9 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
 st.title("Live data stream")
+chart = st.linechart(np.random.rand(1,3))
 
 df = pd.read_csv('data.csv')
 df.columns = ['id', 'time stamp', 'e1', 'e2', 'e3']
 
 st.dataframe(df[['time stamp', 'e1', 'e2', 'e3']])
+
+chart.add_rows(df[['e1', 'e2', 'e3']])
+
